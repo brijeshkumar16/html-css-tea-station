7c330611bc1c88c5c648c3d121a4a482a6daaa6c
Brijeshkumar Yadav <by9714322072@gmail.com>
2024-07-05 12:09:03 +0530

    feat: Create UI of navbar and implement show and hide logic for navbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,13 @@
 const date = document.getElementById('date');
 date.innerHTML = new Date().getFullYear();
+
+const navBtn = document.getElementById('nav-btn');
+const closeBtn = document.getElementById('close-btn');
+const nav = document.getElementById('nav');
+
+navBtn.addEventListener('click', () => {
+  nav.classList.add('showNav');
+});
+closeBtn.addEventListener('click', () => {
+  nav.classList.remove('showNav');
+});
